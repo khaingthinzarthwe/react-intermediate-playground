c37f87bf81149308c402367b06ee92254b24351b
yoh yoh <khaingthinzarthwe.dev@gmail.com>
2024-06-26 11:15:27 +0700

    Parameterized Queries

--- src/react-query/hooks/usePosts.ts
+++ src/react-query/hooks/usePosts.ts
@@ -8,11 +8,16 @@ interface Post {
     userId: number;
 }
 
-const usePosts = () => useQuery<Post[], Error>({
-  queryKey: ["posts"],
+const usePosts = (userId: number | undefined) => useQuery<Post[], Error>({
+  // /users/1/posts
+  queryKey: userId ? ["users", userId, "posts"] : ["posts"],
   queryFn: () =>
     axios
-      .get("https://jsonplaceholder.typicode.com/posts")
+      .get("https://jsonplaceholder.typicode.com/posts", {
+        params: {
+          userId
+        }
+      })
       .then((res) => res.data),
   staleTime: 1 * 60 * 1000 //1m
 });
