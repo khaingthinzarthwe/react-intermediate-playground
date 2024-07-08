116425531b273bf06db83c86e4023374f36478ea
yoh yoh <khaingthinzarthwe.dev@gmail.com>
2024-07-08 15:04:04 +0700

    Exercise-Working with Zustand

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,17 @@
 import "./App.css";
 import NavBar from "./state-management/NavBar";
 import HomePage from "./state-management/HomePage";
-import AuthProvider from "./state-management/auth/AuthProvider";
 import { TasksProvider } from "./state-management/tasks";
 import Counter from "./state-management/counter/Counter";
 
 function App() {
 
   return (
-    <AuthProvider>
       <TasksProvider>
         <Counter />
         <NavBar />
         <HomePage />
       </TasksProvider>
-    </AuthProvider>
   );
 }
 
--- src/state-management/auth/LoginStatus.tsx
+++ src/state-management/auth/LoginStatus.tsx
@@ -1,14 +1,14 @@
-import useAuth from "../auth/useAuth";
+import useAuthStore from "./store";
 
 const LoginStatus = () => {
-  const { user, dispatch } = useAuth();
+  const {user, login, logout} = useAuthStore();
 
   if (user)
     return (
       <>
         <div>
           <span className="mx-2">{user}</span>
-          <a onClick={() => dispatch({ type: "LOGOUT" })} href="#">
+          <a onClick={() => logout()} href="#">
             Logout
           </a>
         </div>
@@ -17,7 +17,7 @@ const LoginStatus = () => {
   return (
     <div>
       <a
-        onClick={() => dispatch({ type: "LOGIN", username: "mosh.hamedani" })}
+        onClick={() => login("mosh.hamedani")}
         href="#"
       >
         Login
